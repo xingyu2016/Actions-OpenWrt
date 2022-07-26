5efe49c1b95e4598adf984f83362323b788214c9
xingyu2016 <tufan888@gmail.com>
2022-07-26 11:14:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,8 +12,7 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
+sed -i 's/routing.git/routing.git;openwrt-21.02/g' feeds.conf.default
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-sed -i 's/routing.git/routing.git;openwrt-21.02/g' feeds.conf.default
